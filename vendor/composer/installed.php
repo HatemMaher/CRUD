d4383e69f7098e30b5448b8e7208a86c68b4e367
Hatem Maher <165530753+HatemMaher@users.noreply.github.com>
2025-07-07 02:46:52 +0300

    2nd sommit

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => 'v12.0.10',
-        'version' => '12.0.10.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'd84e6ba6ee091aa5691ef690d80f44a09575655c',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -386,9 +386,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => 'v12.0.10',
-            'version' => '12.0.10.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'd84e6ba6ee091aa5691ef690d80f44a09575655c',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
